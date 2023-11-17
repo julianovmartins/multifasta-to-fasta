6150810723fa7861933b6b77ff4ab1d7eeb3ca8a
Juliano <julianovmartins@gmail.com>
2023-11-17 15:45:32 -0300

    Removed accessions multifasta_to_fasta.sh

--- multifasta_to_fasta.sh
+++ multifasta_to_fasta.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-for fasta_file in $(find ./ -name '*.fasta'); 
-do 
+for fasta_file in $(find ./ -name '*.fasta'); do
     count_header=`grep -c '^>' $fasta_file`;
     if [[ $count_header -gt 1 ]]; then
         file_name=$(basename "$fasta_file" ".fasta");
@@ -9,8 +8,7 @@ do
         if [ ! -d "$new_directory" ]; then
             mkdir $new_directory;
         fi
-        awk -v dir="$new_directory" -F'[|, ]' '/^>/ {if ($0 ~ /([NC|AC|NG|NT|NW|NS|NZ|NM|NR|XM|XR|AP|NP|YP|XP|ZP]_*)/)  s=$1".fasta"; else s=$0".fasta";} {print > dir"/"s}' $fasta_file;
-        mv $fasta_file $fasta_file".splitted";
+        awk -v dir="$new_directory" '/^>/ {close(F); split(substr($1,2), a, " "); F = dir"/"a[1]".fasta"} {print >> F}' $fasta_file;
         echo "File splitted: $fasta_file";
     fi
 done
